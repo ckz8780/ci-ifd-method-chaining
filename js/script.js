f7926497e0a3be283885c0d0e345e6173104592c
Chris Zielinski <ckz8780@gmail.com>
2018-05-11 02:27:09 +0000

    completed method chaining challenge 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
- //
- // add your jQuery code here
+//
+// add your jQuery code here
 
-
+// Challenge 1:
+ $('#button1').mouseenter(function() {
+  $(this).removeClass("makeRed").addClass('makeBorder');
+ })
+ 
+ $('#button1').mouseleave(function() {
+  $(this).removeClass("makeBorder").addClass('makeRed');
+ });
 }); 
